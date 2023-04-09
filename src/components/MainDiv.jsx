a23336ccc54db8b5c733cf0e0929e2d612f0df86
ljimshitashvili <jimshitashvili4444@gmail.com>
2023-04-09 18:12:57 +0400

    first component

--- src/components/MainDiv.jsx
+++ src/components/MainDiv.jsx
@@ -1,10 +1,21 @@
 import styled from "styled-components";
-import ChildDiv from "./ChildDiv.jsx";
 
 function MainDiv() {
   return (
     <Main>
-      <ChildDiv />
+      <Star>
+        <img
+          src="interactive-rating-component-main (1)/interactive-rating-component-main/images/icon-star.svg"
+          alt="star icon"
+        />
+      </Star>
+      <TextContainer>
+        <h1>How did we do?</h1>
+        <p>
+          Please let us know how we did with your support request. All feedback
+          is appreciated to help us improve our offering!
+        </p>
+      </TextContainer>
     </Main>
   );
 }
@@ -24,4 +35,33 @@ const Main = styled.div`
   flex-direction: column;
   padding: 24px;
   box-sizing: border-box;
+`;
+
+const Star = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background: #262e38;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 50px;
+`;
+
+const TextContainer = styled.div`
+  h1 {
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 30px;
+    color: #ffffff;
+    margin-bottom: 10px;
+  }
+
+  p {
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 22px;
+    color: #969fad;
+    margin-bottom: 24px;
+  }
 `;
